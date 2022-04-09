8c1437199581bb0df59a0b71c5387ea7b9dd3126
miladimos <milad.jafari6210@gmail.com>
2022-04-09 05:14:13 +0430

    edit provider

--- src/Providers/ToasterServiceProvider.php
+++ src/Providers/ToasterServiceProvider.php
@@ -2,15 +2,11 @@
 
 namespace Laravelir\Toaster\Providers;
 
-use App\Http\Kernel;
 use Illuminate\Routing\Router;
 use Laravelir\Toaster\Toaster;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravelir\Http\Middleware\ToasterMiddleware;
-use Laravelir\Toaster\Facades\Toaster as ToasterFacade;
 use Laravelir\Toaster\Console\Commands\InstallPackageCommand;
 
 class ToasterServiceProvider extends ServiceProvider
@@ -36,10 +32,7 @@ class ToasterServiceProvider extends ServiceProvider
         $this->registerCommands();
         // $this->registerTranslations();
         // $this->registerAssets();
-        // $this->registerRoutes();
         // $this->registerBladeDirectives();
-        // $this->publishStubs();
-        // $this->registerLivewireComponents();
     }
 
     private function registerFacades()
@@ -48,16 +41,6 @@ class ToasterServiceProvider extends ServiceProvider
             return new Toaster();
         });
     }
-
-    // private function registerViews()
-    // {
-    //     $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'toaster');
-
-    //     $this->publishes([
-    //         __DIR__ . '/../../resources/views' => resource_path('views/laravelir/toaster'),
-    //     ]);
-    // }
-
 
     private function registerCommands()
     {
@@ -79,25 +62,25 @@ class ToasterServiceProvider extends ServiceProvider
     // private function registerAssets()
     // {
     //     $this->publishes([
-    //         __DIR__ . '/../../resources/statics' => public_path('dashboarder'),
-    //     ], 'dashboarder-assets');
+    //         __DIR__ . '/../../resources/statics' => public_path('toaster'),
+    //     ], 'toaster-assets');
     // }
 
 
     public function registerTranslations()
     {
-        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'dashboarder');
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'toaster');
 
         $this->publishes([
-            __DIR__ . '/../../resources/lang' => resource_path('lang/laravelir/dashboarder'),
-        ], 'dashboarder-langs');
+            __DIR__ . '/../../resources/lang' => resource_path('lang/laravelir/toaster'),
+        ], 'toaster-langs');
     }
 
 
     protected function registerBladeDirectives()
     {
         Blade::directive('toaster_config', function ($key) {
-            return "<?php echo config('dashboarder.' . $key); ?/>";
+            return "<?php echo config('toaster.' . $key); ?/>";
         });
     }
 
